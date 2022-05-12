4ea682ee0d54afb600b615aaa2d8711c9c65c846
Tina Hwang <jingyih1999@gmail.com>
2022-05-12 13:11:26 -0400

    add required packages

--- vofromscratch.py
+++ vofromscratch.py
@@ -1,57 +1,14 @@
 from glob import glob
+import numpy as np
 import cv2, skimage, os, scipy
 import scipy.spatial
-import numpy as np
+from scipy import signal as sig
+from numpy.lib.stride_tricks import sliding_window_view
+from base import VO
 
-
-class VO:
-    def __init__(self, frame_path):
-        self.frame_path = frame_path
-        self.images = []
-        self.pos = []
-        self.frames = sorted(glob(os.path.join(self.frame_path, 'images', '*')))
-        with open(os.path.join(frame_path, 'calib.txt'), 'r') as f:
-            lines = f.readlines()
-            self.focal_length = float(lines[0].strip().split()[-1])
-            lines[1] = lines[1].strip().split()
-            self.pp = (float(lines[1][1]), float(lines[1][2]))
-            
-        with open(os.path.join(self.frame_path, 'gt_sequence.txt'), 'r') as f:
-            self.pose = [line.strip().split() for line in f.readlines()]
-
-    def imread(self, fname):
-        """
-        read image into np array from file
-        """
-        return cv2.imread(fname)
-
-    def imread_bw(self, fname):
-        """
-        read image as gray scale format
-        """
-        return cv2.cvtColor(self.imread(fname), cv2.COLOR_BGR2GRAY)
-
-    def imshow(self, img):
-        """
-        show image
-        """
-        skimage.io.imshow(img)
-
-    def get_gt(self, frame_id):
-        pose = self.pose[frame_id]
-        x, y, z = float(pose[3]), float(pose[7]), float(pose[11])
-        return np.array([[x], [y], [z]])
-
-    def get_scale(self, frame_id):
-        '''Provides scale estimation for mutliplying
-        translation vectors
-        
-        Returns:
-        float -- Scalar value allowing for scale estimation
-        '''
-        prev_coords = self.get_gt(frame_id - 1)
-        curr_coords = self.get_gt(frame_id)
-        return np.linalg.norm(curr_coords - prev_coords)
+class Odometry(VO):
+    def __init__(self, frame_path) -> None:
+        super().__init__(frame_path)
 
     def get_sift_data(self, img):
         """
@@ -246,8 +203,6 @@ class VO:
 
         kp1 = kp1[status].reshape(-1, 2)
         kp2 = kp2[status].reshape(-1, 2)
-
-        kp1, kp2 = self.ransac(kp1, kp2, thres=1)
         
         return kp1, kp2
     
